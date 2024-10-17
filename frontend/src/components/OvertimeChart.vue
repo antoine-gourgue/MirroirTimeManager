7546e6de8b2f4208b657a41341473cce3b7685b3
Antoine Labard <antoine@labard.fr>
2024-10-17 11:51:43 +0200

    implement a first version of the OvertimeChart widget

---
+++ frontend/src/components/OvertimeChart.vue
@@ -0,0 +1,86 @@
+<template>
+  <div>
+    <canvas ref="myChart"></canvas>
+  </div>
+</template>
+
+<script setup>
+import {Chart, registerables} from 'chart.js';
+import {onMounted, ref} from 'vue';
+
+Chart.register(...registerables);
+
+const props = defineProps({
+  progress: {
+    type: Number,
+    required: true
+  }
+})
+
+const myChart = ref(null)
+const chartData = {
+  labels: ['Progress'],
+  datasets: [
+    {
+      label: 'Progress',
+      data: [props.progress],
+      backgroundColor: 'black',
+      borderColor: 'white',
+      borderWidth: 1,
+    },
+  ],
+}
+const chartOptions = {
+  responsive: true,
+  plugins: {
+    legend: {
+      display: false
+    }
+  },
+  indexAxis: "y",
+  scales: {
+    x: {
+      min: 0,
+      max: 100,
+      title: {
+        display: true,
+        text: 'Daytime overtime done',
+      },
+      grid: {
+        display: false,
+        drawBorder: false
+      },
+      ticks: {
+        display: false
+      }
+    },
+    y: {
+      beginAtZero: true,
+      ticks: {
+        display: false
+      },
+      grid: {
+        display: false,
+        drawBorder: false
+      }
+    },
+  },
+}
+
+onMounted(() => {
+  const ctx = myChart.value.getContext('2d');
+  new Chart(ctx, {
+    type: 'bar',
+    data: chartData,
+    options: chartOptions,
+  });
+});
+</script>
+
+<style scoped>
+Bar {
+  position: relative;
+  width: 100%;
+  height: 50px;
+}
+</style>
