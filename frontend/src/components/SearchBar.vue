cf9eba870c0285776f5caad2302fbbe7fba46574
Moonshield7 <fannie.p@hotmail.fr>
2024-10-17 12:20:00 +0200

    💄 Creating the searchbar component

---
+++ frontend/src/components/SearchBar.vue
@@ -0,0 +1,41 @@
+<script setup>
+const props = defineProps({placeholder: String})
+
+console.log(props.placeholder);
+
+</script>
+
+<template>
+  <div class="searchbar-container">
+    <input type="text" placeholder="Temporary placeholder" class="searchbar">
+    <img src="../assets/search.svg" alt="" class="searchbar-icon">
+  </div>
+
+</template>
+
+<style>
+.searchbar-container {
+  height: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.searchbar {
+  width: 300px;
+  height: 50px;
+  margin-top: 15px;
+  border-radius: 25px;
+  padding: 15px;
+}
+
+.searchbar-icon{
+height: 25px;
+position: relative;
+left: -45px;
+top:6px
+}
+
+.searchbar-icon:hover{
+  transform: scale(110%);
+}
+</style>
