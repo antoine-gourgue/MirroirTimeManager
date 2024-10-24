6d686a1ef93d4e7e7a495330f4f35b29efc95794
Antoine <antoine.gourgue@epitch.eu>
2024-10-24 13:22:39 +0200

    fix: delete team

--- backend/lib/time_manager_web/controllers/team_controller.ex
+++ backend/lib/time_manager_web/controllers/team_controller.ex
@@ -17,21 +17,23 @@ defmodule TimeManagerWeb.TeamController do
     render(conn, "index.json", teams: teams)
   end
 
+  # Créer une nouvelle équipe
   def create(conn, %{"team" => team_params}) do
     case Accounts.create_team(team_params) do
       {:ok, team} ->
         conn
         |> put_status(:created)
-        |> put_resp_header("location", ~p"/teams/#{team.id}")  # Utilisation du sigil ~p pour générer la route
+        |> put_resp_header("location", ~p"/teams/#{team.id}")
         |> render("show.json", team: team)
 
       {:error, _reason} ->
         conn
         |> put_status(:unprocessable_entity)
-        |> json(%{error: "Invalid manager ID"})  # Simplification du message d'erreur
+        |> json(%{error: "Invalid manager ID"})
     end
   end
 
+  # Mettre à jour une équipe existante
   def update(conn, %{"id" => id, "team" => team_params}) do
     team = Accounts.get_team!(id)
 
@@ -43,6 +45,20 @@ defmodule TimeManagerWeb.TeamController do
         conn
         |> put_status(:unprocessable_entity)
         |> json(TimeManagerWeb.TeamJSON.error(%{changeset: changeset}))
+    end
+  end
+
+  # Supprimer une équipe
+  def delete(conn, %{"id" => id}) do
+    team = Accounts.get_team!(id)
+
+    case Accounts.delete_team(team) do
+      {:ok, _team} ->
+        send_resp(conn, :no_content, "")
+      {:error, _reason} ->
+        conn
+        |> put_status(:unprocessable_entity)
+        |> json(%{error: "Unable to delete team"})
     end
   end
 end
