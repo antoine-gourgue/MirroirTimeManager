2c4dd4114843d698d2d59742a3fb3a3d00ec057f
Antoine <antoine.gourgue@epitch.eu>
2024-10-24 14:08:19 +0200

    test: fix test for user and user team

--- backend/test/support/fixtures/accounts_fixtures.ex
+++ backend/test/support/fixtures/accounts_fixtures.ex
@@ -61,19 +61,28 @@ defmodule TimeManager.AccountsFixtures do
   Generate a user_team.
   """
   def user_team_fixture(attrs \\ %{}) do
-    # Vous pouvez ici passer un `user` et un `team` déjà créés si nécessaire, sinon en créer de nouveaux
-    user = Map.get(attrs, :user, user_fixture())
-    team = Map.get(attrs, :team, team_fixture())
+    user = case attrs[:user] do
+      nil -> user_fixture() # Créer un utilisateur si aucun n'est fourni
+      _user -> attrs[:user] # Utiliser l'utilisateur fourni
+    end
 
-    # Fusion des attributs passés avec les valeurs par défaut
+    team = case attrs[:team] do
+      nil -> team_fixture() # Créer une équipe si aucune n'est fournie
+      _team -> attrs[:team] # Utiliser l'équipe fournie
+    end
+
+    # Fusionner les attributs passés avec les valeurs par défaut
     merged_attrs = attrs
                    |> Enum.into(%{
       user_id: user.id,
       team_id: team.id
     })
 
+    # Créer le `user_team`
     {:ok, user_team} = TimeManager.Accounts.create_user_team(merged_attrs)
+
     user_team
   end
+
 
 end
